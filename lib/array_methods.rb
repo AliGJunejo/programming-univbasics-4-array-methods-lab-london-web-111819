f10accf2e47d0f2f897ad17f75a2d1cc332996c9
Ali G Junejo <aligohar_junejo@live.com>
2019-11-14 15:00:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,14 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  using_sort.sort
+  sorted = using_sort
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
